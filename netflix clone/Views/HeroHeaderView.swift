27fd2f8a368f54e9b210ea4c9682007697b81cf3
Jasleen <jasleen3.work@gmail.com>
2024-01-30 14:05:22 +0530

    chages

---
+++ netflix clone/Views/HeroHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  HeroHeaderView.swift
+//  netflix clone
+//
+//  Created by 1 on 25/01/24.
+//
+
+import UIKit
+
+class HeroHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
